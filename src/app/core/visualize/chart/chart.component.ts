c424c5cfc33ccbae2ba8162ac816c57f62c7c0a1
Kaali <inderdeepbajwa@live.com>
2019-04-16 16:37:59 -0400

    D3 Sankey implementation

--- src/app/core/visualize/chart/chart.component.ts
+++ src/app/core/visualize/chart/chart.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { DataDriverService } from '../data-driver.service';
 
+import * as d3 from 'd3';
+import * as d3sankey from 'd3-sankey';
+
 @Component({
   selector: 'app-chart',
   templateUrl: './chart.component.html',
@@ -10,10 +13,17 @@ import { DataDriverService } from '../data-driver.service';
 export class ChartComponent implements OnInit {
   message: string;
 
+  chart: any;
+
+  chartData: DAG;
+
+  nodes; links;
+
   constructor(private data: DataDriverService, private router: Router) { }
 
-  ngOnInit() {
+  ngOnInit() : void {
     this.data.currentMessage.subscribe(message => this.message = message);
+    this.drawChart();
   }
 
   newMessage(chartData: string) {
@@ -32,4 +42,93 @@ export class ChartComponent implements OnInit {
     // data3 contains sufficient data
   }
 
+  public drawChart() {
+    
+    var svg = d3.select("#sankey"),
+        width = +svg.attr("width"),
+        height = +svg.attr("height");
+
+    var formatNumber = d3.format(",.0f"),
+        format = function (d: any) { return formatNumber(d) + " TWh"; },
+        color = d3.scaleOrdinal(d3.schemeCategory10);
+
+    var sankey = d3sankey.sankey()
+        .nodeWidth(15)
+        .nodePadding(10)
+        .extent([[1, 1], [width - 1, height - 6]]);
+
+    var link = svg.append("g")
+        .attr("class", "links")
+        .attr("fill", "none")
+        .attr("stroke", "#000")
+        .attr("stroke-opacity", 0.2)
+        .selectAll("path");
+
+    var node = svg.append("g")
+        .attr("class", "nodes")
+        .attr("font-family", "sans-serif")
+        .attr("font-size", 10)
+        .selectAll("g");
+        
+    d3.json("//cdn.rawgit.com/q-m/d3.chart.sankey/master/example/data/product.json")
+        .then(function(data: DAG) {
+      // @ts-ignore
+      sankey(data);
+      link = link
+          .data(data.links)
+          .enter().append("path")
+          .attr("d", d3sankey.sankeyLinkHorizontal())
+          .attr("stroke-width", function (d: any) { return Math.max(1, d.width); });
+
+      link.append("title")
+          .text(function (d: any) { return d.source.name + " → " + d.target.name + "\n" + format(d.value); });
+
+      node = node
+          .data(data.nodes)
+          .enter().append("g");
+
+      node.append("rect")
+          .attr("x", function (d: any) { return d.x0; })
+          .attr("y", function (d: any) { return d.y0; })
+          .attr("height", function (d: any) { return d.y1 - d.y0; })
+          .attr("width", function (d: any) { return d.x1 - d.x0; })
+          .attr("fill", function (d: any) { return color(d.name.replace(/ .*/, "")); })
+          .attr("stroke", "#000");
+
+      node.append("text")
+          .attr("x", function (d: any) { return d.x0 - 6; })
+          .attr("y", function (d: any) { return (d.y1 + d.y0) / 2; })
+          .attr("dy", "0.35em")
+          .attr("text-anchor", "end")
+          .text(function (d: any) { return d.name; })
+          .filter(function (d: any) { return d.x0 < width / 2; })
+          .attr("x", function (d: any) { return d.x1 + 6; })
+          .attr("text-anchor", "start");
+
+      node.append("title")
+          .text(function (d: any) { return d.name + "\n" + format(d.value); });
+
+    });
+  }
+
+
+}
+
+interface SNodeExtra {
+  nodeId: number;
+  name: string;
+}
+
+interface SLinkExtra {
+  source: number;
+  target: number;
+  value: number;
+  uom: string;
+}
+type SNode = d3sankey.SankeyNode<SNodeExtra, SLinkExtra>;
+type SLink = d3sankey.SankeyLink<SNodeExtra, SLinkExtra>;
+
+interface DAG {
+  nodes: SNode[];
+  links: SLink[];
 }
--- src/app/core/visualize/template/prevchart/prevchart.component.ts
+++ src/app/core/visualize/template/prevchart/prevchart.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import * as d3 from 'd3';
 import * as d3Sankey from 'd3-sankey';
 import { DataDriverService } from '../../data-driver.service';
-import { GoogleChartComponent, GoogleChartsModule } from 'angular-google-charts';
 
 @Component({
   selector: 'app-prevchart',
@@ -17,147 +16,69 @@ export class PrevchartComponent implements OnInit {
   constructor(private receivedData: DataDriverService) { }
   
   ngOnInit() {
-    this.receivedData.currentMessage.subscribe(message => this.onChangeMessage(message));
-    google.charts.setOnLoadCallback(this.drawChart());
-    // data3 contains sufficient data
+    this.receivedData.currentMessage.subscribe(message => this.message = message);
   }
 
   chartData = [
-    ["From", "To", 3],
-    ["From", "Towards", 1]
-  ];
-  
-  drawChart() {
-    // Define the chart to be drawn.
-    var data = new google.visualization.DataTable();   
-    data.addColumn('string', 'From');
-    data.addColumn('string', 'To');
-    data.addColumn('number', 'Weight');
-
-    data.addRows([
-       [ 'Brazil', 'Portugal', 5 ],
-       [ 'Brazil', 'France', 1 ],
-       [ 'Brazil', 'Spain', 1 ],
-       [ 'Brazil', 'England', 1 ],
-       
-       [ 'Canada', 'Portugal', 1 ],
-       [ 'Canada', 'France', 5 ],
-       [ 'Canada', 'England', 1 ],
-       
-       [ 'Mexico', 'Portugal', 1 ],
-       [ 'Mexico', 'France', 1 ],
-       [ 'Mexico', 'Spain', 5 ],
-       [ 'Mexico', 'England', 1 ],
-       
-       [ 'USA', 'Portugal', 1 ],
-       [ 'USA', 'France', 1 ],
-       [ 'USA', 'Spain', 1 ],
-       [ 'USA', 'England', 5 ],
-       
-       [ 'Portugal', 'Angola', 2 ],
-       [ 'Portugal', 'Senegal', 1 ],
-       [ 'Portugal', 'Morocco', 1 ],
-       [ 'Portugal', 'South Africa', 3 ],
-       
-       [ 'France', 'Angola', 1 ],
-       [ 'France', 'Senegal', 3 ],
-       [ 'France', 'Mali', 3 ],
-       [ 'France', 'Morocco', 3 ],
-       [ 'France', 'South Africa', 1 ],
-       
-       [ 'Spain', 'Senegal', 1 ],
-       [ 'Spain', 'Morocco', 3 ],
-       [ 'Spain', 'South Africa', 1 ],
-       
-       [ 'England', 'Angola', 1 ],
-       [ 'England', 'Senegal', 1 ],
-       [ 'England', 'Morocco', 2 ],
-       [ 'England', 'South Africa', 7 ],
-       
-       [ 'South Africa', 'China', 5 ],
-       [ 'South Africa', 'India', 1 ],
-       [ 'South Africa', 'Japan', 3 ],
-       
-       [ 'Angola', 'China', 5 ],
-       [ 'Angola', 'India', 1 ],
-       [ 'Angola', 'Japan', 3 ],
-       
-       [ 'Senegal', 'China', 5 ],
-       [ 'Senegal', 'India', 1 ],
-       [ 'Senegal', 'Japan', 3 ],
-       
-       [ 'Mali', 'China', 5 ],
-       [ 'Mali', 'India', 1 ],
-       [ 'Mali', 'Japan', 3 ],
-       
-       [ 'Morocco', 'China', 5 ],
-       [ 'Morocco', 'India', 1 ],
-       [ 'Morocco', 'Japan', 3 ]
-    ]);	
-
-    // Set chart options
-    var options = {width: 550};
-          
-    // Instantiate and draw the chart.
-    var chart = new google.visualization.Sankey(document.getElementById('container'));
-    console.log("THING " + chart);
-    chart.draw(data, options);
- }
- 
- 
+    [ 'Brazil', 'Portugal', 5 ],
+    [ 'Brazil', 'France', 1 ],
+    [ 'Brazil', 'Spain', 1 ],
+    [ 'Brazil', 'England', 1 ],
+    
+    [ 'Canada', 'Portugal', 1 ],
+    [ 'Canada', 'France', 5 ],
+    [ 'Canada', 'England', 1 ],
+    
+    [ 'Mexico', 'Portugal', 1 ],
+    [ 'Mexico', 'France', 1 ],
+    [ 'Mexico', 'Spain', 5 ],
+    [ 'Mexico', 'England', 1 ],
+    
+    [ 'USA', 'Portugal', 1 ],
+    [ 'USA', 'France', 1 ],
+    [ 'USA', 'Spain', 1 ],
+    [ 'USA', 'England', 5 ],
+    
+    [ 'Portugal', 'Angola', 2 ],
+    [ 'Portugal', 'Senegal', 1 ],
+    [ 'Portugal', 'Morocco', 1 ],
+    [ 'Portugal', 'South Africa', 3 ],
+    
+    [ 'France', 'Angola', 1 ],
+    [ 'France', 'Senegal', 3 ],
+    [ 'France', 'Mali', 3 ],
+    [ 'France', 'Morocco', 3 ],
+    [ 'France', 'South Africa', 1 ],
+    
+    [ 'Spain', 'Senegal', 1 ],
+    [ 'Spain', 'Morocco', 3 ],
+    [ 'Spain', 'South Africa', 1 ],
+    
+    [ 'England', 'Angola', 1 ],
+    [ 'England', 'Senegal', 1 ],
+    [ 'England', 'Morocco', 2 ],
+    [ 'England', 'South Africa', 7 ],
+    
+    [ 'South Africa', 'China', 5 ],
+    [ 'South Africa', 'India', 1 ],
+    [ 'South Africa', 'Japan', 3 ],
+    
+    [ 'Angola', 'China', 5 ],
+    [ 'Angola', 'India', 1 ],
+    [ 'Angola', 'Japan', 3 ],
+    
+    [ 'Senegal', 'China', 5 ],
+    [ 'Senegal', 'India', 1 ],
+    [ 'Senegal', 'Japan', 3 ],
+    
+    [ 'Mali', 'China', 5 ],
+    [ 'Mali', 'India', 1 ],
+    [ 'Mali', 'Japan', 3 ],
+    
+    [ 'Morocco', 'China', 5 ],
+    [ 'Morocco', 'India', 1 ],
+    [ 'Morocco', 'Japan', 3 ]
+ ];
 
 
-  public onChangeMessage(cData: string) {
-    // console.log(this.options.height);
-    this.message = cData;
-    if(this.message !== this.previousMessage) {
-      // console.log(this.chartData);
-      // var data2 = this.message.split('\n');
-      // this.chartData = data2.map((value) => value.split(','));
-      // console.log(this.chartData);
-      // this.data = this.chartData;
-      // this.options.height = 100;
-      // console.log(this.options.height);
-      this.previousMessage = this.message;
-
-      this.receivedData.currentMessage.subscribe(message => this.message = message);
-      var data = this.message.split('\n');
-      let data2 = data.map((value) => value.split(', '));
-      var jsonStr = JSON.stringify(data2);
-      console.log(jsonStr);
-      jsonStr = jsonStr.replace(/"v":"(\d+)"/g, '"v":$1').replace(/"f":"(\d+)"/g, '+"f":$1');
-
-      let data3 = JSON.parse(jsonStr);
-      for (var i = 0; i < data3.length; i++) {
-        if (isNaN(data3[i])) {
-          this.chartData.push(data3[i]);
-        } else {
-          // @ts-ignore
-          this.chartData.push(+data3[i]);
-        }
-      }
-      this.data = this.chartData;
-    }
-  }
-
-  
-
-  // To manage the size of preview (yet to implement)
-  public increaseSize() {
-    this.width = 400;
-    this.height = 1000;
-  }
-
-  public decreaseSize() {
-    this.width = 270;
-    this.height = 400;
-  }
-
-  // public getData(data:string) {
-  //   console.log(data);
-
-  //   var data2 = data.split('\n');
-  //   var data3 = data2.map((value) => value.split(','));
-  //   console.log(data3);
-  // }
 }
