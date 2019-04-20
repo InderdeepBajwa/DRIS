aabb507497b5fc5e69ceed0373532083bce263c2
Kaali <inderdeepbajwa@live.com>
2019-04-19 22:41:47 -0400

    Sankey gradient implementation

--- src/app/core/visualize/chart/chart.component.ts
+++ src/app/core/visualize/chart/chart.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 import { Router } from '@angular/router';
 import { DataDriverService } from '../data-driver.service';
 
@@ -13,27 +13,28 @@ import * as d3sankey from 'd3-sankey';
 export class ChartComponent implements OnInit {
   message: string;
 
-  chart: any;
-
-  chartData: DAG;
-
-  nodes; links;
-
   constructor(private data: DataDriverService, private router: Router) { }
 
   ngOnInit() : void {
     this.data.currentMessage.subscribe(message => this.message = message);
-    this.drawChart();
+    // TODO: DO IT this.drawChart();
   }
 
   newMessage(chartData: string) {
-    this.data.changeMessage(chartData);
+    this.data.changeMessage("source,target,value\n" + chartData);
+    this.drawChart();
   }
 
   visualize(chartData: string) {
-    this.data.changeMessage(chartData);
+    this.data.changeMessage("source,target,value\n" + chartData);
     this.router.navigateByUrl('visualize');
 
+  }
+
+  // Manages key presses in textarea (sankey typing)
+  public keyPressed(event: any) {
+    this.message = "source,target,value\n" + event.target.value;
+    this.drawChart();
   }
 
   public getData(data:string) {
@@ -43,15 +44,37 @@ export class ChartComponent implements OnInit {
   }
 
   public drawChart() {
-    
+
+    // Error checking for null value
+    if (this.message == "" || this.message == null) {
+      return;
+    }
+
+    // Color scheme
+    const colors = d3.scaleOrdinal()
+        .domain([
+          'red',
+          'green'
+        ])
+        .range([
+          '#ffb2a8',
+          '#89ef56'
+        ]);
+
+    // Selecting Sankey element from HTML
     var svg = d3.select("#sankey"),
         width = +svg.attr("width"),
         height = +svg.attr("height");
 
+    // Clearing previous graph
+    svg.selectAll("*").remove();
+
+    // Formatting numbers in Sankey
     var formatNumber = d3.format(",.0f"),
-        format = function (d: any) { return formatNumber(d) + " TWh"; },
+        format = function (d: any) { return formatNumber(d) }, //+ " Units"; }, Optional units
         color = d3.scaleOrdinal(d3.schemeCategory10);
 
+    // Initializing Sankey variable
     var sankey = d3sankey.sankey()
         .nodeWidth(15)
         .nodePadding(10)
@@ -60,37 +83,38 @@ export class ChartComponent implements OnInit {
     var link = svg.append("g")
         .attr("class", "links")
         .attr("fill", "none")
-        .attr("stroke", "#000")
-        .attr("stroke-opacity", 0.2)
-        .selectAll("path");
+        .attr("stroke", "none")
+        .attr("stroke-opacity", 1)
+        .selectAll("path")
+        .on('mouseover', function() {
+          d3.select(this).style('stroke-opacity', 0.5);
+        })
+        .on('mouseout', function() {
+          d3.select(this).style('stroke-opacity', 0.2);
+        });
+  
 
     var node = svg.append("g")
         .attr("class", "nodes")
         .attr("font-family", "sans-serif")
         .attr("font-size", 10)
         .selectAll("g");
-      
-
-    var graph = { "nodes" : [], "links" : [] };
     
-    var cData = "";
+    
+    // Initializing graph (data container)
+    var graph = { "nodes" : [], "links" : [] };
 
-    var sankeyData = d3.csvParse(`source,target,value
-red,yellow,20
-red,blue,30
-blue,green,5`);
-    console.log(sankeyData);
-          
+    var sankeyData = d3.csvParse(this.message);
+
     sankeyData.forEach(d => {
       graph.nodes.push({ "name": d.source});
       graph.nodes.push({ "name": d.target});
       graph.links.push({ "source" : d.source,
                         "target" : d.target,
                         "value": +d.value });
+    });
 
     graph.nodes = d3.map(graph.nodes, function(d) {return d.name;}).keys();
-    console.log("CSV DATA HERE");
-    console.log(graph);
 
     graph.links.forEach(function (d, i) {
       graph.links[i].source = graph.nodes.indexOf(graph.links[i].source);
@@ -100,72 +124,94 @@ blue,green,5`);
     graph.nodes.forEach(function (d, i) {
       graph.nodes[i] = { "name": d };
     });
-
+    
     sankey(graph);
-    // });
 
-
+    // Color definitions
+    const defs = svg.append('defs');
           
-    // d3.json("//cdn.rawgit.com/q-m/d3.chart.sankey/master/example/data/product.json")
-    //     .then(function(data: DAG) {
-    //       console.log(data);
-    //   // @ts-ignore
-    //   sankey(data);
-      link = link
-          .data(graph.links)
-          .enter().append("path")
-          .attr("d", d3sankey.sankeyLinkHorizontal())
-          .attr("stroke-width", function (d: any) { return Math.max(1, d.width); });
+    link = link
+        .data(graph.links)
+        .enter().append("path")
+        .attr("d", d3sankey.sankeyLinkHorizontal())
+        .attr("stroke-width", function (d: any) { return Math.max(1, d.width); });
 
-      link.append("title")
-          .text(function (d: any) { return d.source.name + " → " + d.target.name + "\n" + format(d.value); });
+    link.append("title")
+        .text(function (d: any) { return d.source.name + " → " + d.target.name + "\n" + format(d.value); });
 
-      node = node
-          .data(graph.nodes)
-          .enter().append("g");
+    node = node
+        .data(graph.nodes)
+        .enter().append("g");
 
-      node.append("rect")
-          .attr("x", function (d: any) { return d.x0; })
-          .attr("y", function (d: any) { return d.y0; })
-          .attr("height", function (d: any) { return d.y1 - d.y0; })
-          .attr("width", function (d: any) { return d.x1 - d.x0; })
-          .attr("fill", function (d: any) { return color(d.name.replace(/ .*/, "")); })
-          .attr("stroke", "#000");
+    node.append("rect")
+        .attr("x", function (d: any) { return d.x0; })
+        .attr("y", function (d: any) { return d.y0; })
+        .attr("height", function (d: any) { return d.y1 - d.y0; })
+        .attr("width", function (d: any) { return d.x1 - d.x0; })
+        .style("fill", function (d: any) { 
+          if (colors.domain().indexOf(d.name) > -1) {
+            return d.color = colors(d.name);
+          } else {
+            return d.color = getRandomColor();
+          }
+         });
 
-      node.append("text")
-          .attr("x", function (d: any) { return d.x0 - 6; })
-          .attr("y", function (d: any) { return (d.y1 + d.y0) / 2; })
-          .attr("dy", "0.35em")
-          .attr("text-anchor", "end")
-          .text(function (d: any) { return d.name; })
-          .filter(function (d: any) { return d.x0 < width / 2; })
-          .attr("x", function (d: any) { return d.x1 + 6; })
-          .attr("text-anchor", "start");
+    node.append("text")
+        .attr("x", function (d: any) { return d.x0 - 6; })
+        .attr("y", function (d: any) { return (d.y1 + d.y0) / 2; })
+        .attr("dy", "0.35em")
+        .style('fill', 'white')
+        .style('font-weight', 'bold')
+        .style('font-size', '14px')
+        .attr("text-anchor", "end")
+        .text(function (d: any) { return d.name + " [" + format(d.value) + ']'; })
+        .filter(function (d: any) { return d.x0 < width / 2; })
+        .attr("x", function (d: any) { return d.x1 + 6; })
+        .attr("text-anchor", "start");
 
-      node.append("title")
-          .text(function (d: any) { return d.name + "\n" + format(d.value); });
+    link.style('stroke', (d,i) => {
+      
+      // Unique gradient ID
+      const gradientID = `gradient${i}`;
+
+      const startColor = d.source.color;
+      const stopColor = d.target.color;
+
+      const linearGradient = defs.append('linearGradient')
+            .attr('id', gradientID);
+      
+      linearGradient.selectAll('stop').data([
+          {offset: '10%', color: startColor },      
+          {offset: '90%', color: stopColor } 
+      ])
+      .enter().append('stop')
+      .attr('offset', d => {
+        return d.offset; 
+      })   
+      .attr('stop-color', d => {
+        return d.color;
+      });
+      
+      return `url(#${gradientID})`;
 
     });
+
+    node.append("title")
+        .text(function (d: any) { return d.name + "\n" + format(d.value); });
+    
+    
+    // Utility functions
+    
+    // Method to generate random color
+    function getRandomColor() {
+      var letters = '123456789ABCDEF';
+      var color = '#';
+      for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+      }
+      return color;
+    }
   }
 
-
 }
 
-interface SNodeExtra {
-  nodeId: number;
-  name: string;
-}
-
-interface SLinkExtra {
-  source: number;
-  target: number;
-  value: number;
-  uom: string;
-}
-type SNode = d3sankey.SankeyNode<SNodeExtra, SLinkExtra>;
-type SLink = d3sankey.SankeyLink<SNodeExtra, SLinkExtra>;
-
-interface DAG {
-  nodes: SNode[];
-  links: SLink[];
-}
--- src/app/core/visualize/visualize.component.ts
+++ src/app/core/visualize/visualize.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { DataDriverService } from './data-driver.service';
-import {} from 'angular-google-charts';
+import * as d3 from 'd3';
+import * as d3sankey from 'd3-sankey';
+import saveSvgAsPng from 'save-svg-as-png';
 
 @Component({
   selector: 'app-visualize',
@@ -9,92 +11,136 @@ import {} from 'angular-google-charts';
 })
 export class VisualizeComponent implements OnInit {
   message: string;
-  chartData: (string | number)[][] = [["___", "_______", 0.0]];
   
   
   width = 1200;
   height = 600;
 
   constructor(private receivedData: DataDriverService) {
-   }
+  }
 
   ngOnInit() {
     this.receivedData.currentMessage.subscribe(message => this.message = message);
-    var data = this.message.split('\n');
-    var data2 = data.map((value) => value.split(', '));
-    var jsonStr = JSON.stringify(data2);
-    console.log(jsonStr);
-    jsonStr = jsonStr.replace(/"v":"(\d+)"/g, '"v":$1').replace(/"f":"(\d+)"/g, '+"f":$1');
-
-    let data3 = JSON.parse(jsonStr);
-    for (var i = 0; i < data3.length; i++) {
-      if (isNaN(data3[i])) {
-        this.chartData.push(data3[i]);
-      } else {
-        // @ts-ignore
-        this.chartData.push(+data3[i]);
-      }
-    }
-
-    // Calculating totals
-    var sourceTotals = {};
-
-    for (var i = 0; i < this.chartData.length; i++) {
-        var item = this.chartData[i],
-            key = item[0];
-        if (sourceTotals.hasOwnProperty(key)) {
-            sourceTotals[key] += (+item[2]);
-        }
-        else {
-            sourceTotals[key] = (+item[2]);
-        }
-    }
-
-    var destTotals = {};
-
-    for (var i = 0; i < this.chartData.length; i++) {
-        var item = this.chartData[i],
-            key = item[1];
-        if (destTotals.hasOwnProperty(key)) {
-            destTotals[key] += item[2];
-        }
-        else {
-            destTotals[key] = item[2];
-        }
-    }
-    this.chartData.forEach( function(d) {
-      d[0] = d[0] + " [" + sourceTotals[d[0]] + "]";
-      d[1] = d[1] + " [" + destTotals[d[1]] + "]";
-    })
-    //this.chartData = data3;
+    this.drawChart();
   }
 
-  // Drawing Sankey
-  title = '';
-  type = 'Sankey';
-  data = this.chartData;
-  columnNames = ['From', 'To','Weight'];
-  color = ['#a6cee3', '#b2df8a', '#fb9a99', '#fdbf6f', 
-      '#cab2d6', '#ffff99', '#1f78b4', '#33a02c',
-      '#36cfe3', '#beafea', '#eb9a19', '#0dff6f', 
-      '#c3b2a6', '#f2af29', '#a578a4', '#baa02c'];
+  public drawChart() {
 
-  options = {
+    // Error checking for null value
+    if (this.message == "" || this.message == null) {
+      return;
+    }
     
-   height: this.height,
-   sankey: {
-     node: {
-        interactivity: true,
-        colors: this.color
-      },
-      link: {
-        colorMode: 'gradient',
-        colors: this.color
-      }
-   }
- };
+    // Color scheme
+    const colors = d3.scaleOrdinal()
+      .domain([
+        'red',
+        'green'
+      ])
+      .range([
+        '#ffb2a8',
+        '#89ef56'
+      ]);
+    
+    
 
-  // Save Image Function
-  
+    
 
+    // Selecting Sankey element from HTML
+    var svg = d3.select("#sankey"),
+        width = +svg.attr("width"),
+        height = +svg.attr("height");
+
+    // Clearing previous graph
+    svg.selectAll("*").remove();
+
+    // Formatting numbers in Sankey
+    var formatNumber = d3.format(",.0f"),
+        format = function (d: any) { return formatNumber(d) }, // (Optional) to add functionality of units: + " TWh"; },
+        color = d3.scaleOrdinal(d3.schemeCategory10);
+
+    // Initializing Sankey variable
+    var sankey = d3sankey.sankey()
+        .nodeWidth(15)
+        .nodePadding(10)
+        .extent([[1, 1], [width - 1, height - 6]]);
+
+    var link = svg.append("g")
+        .attr("class", "links")
+        .attr("fill", "none")
+        .attr("stroke", "#000")
+        .attr("stroke-opacity", 0.2)
+        .selectAll("path");
+
+    var node = svg.append("g")
+        .attr("class", "nodes")
+        .attr("font-family", "sans-serif")
+        .attr("font-size", 10)
+        .selectAll("g");
+    
+    // Initializing graph (data container)
+    var graph = { "nodes" : [], "links" : [] };
+
+    var sankeyData = d3.csvParse(this.message);
+
+    sankeyData.forEach(d => {
+      graph.nodes.push({ "name": d.source});
+      graph.nodes.push({ "name": d.target});
+      graph.links.push({ "source" : d.source,
+                        "target" : d.target,
+                        "value": +d.value });
+    });
+
+    graph.nodes = d3.map(graph.nodes, function(d) {return d.name;}).keys();
+
+    graph.links.forEach(function (d, i) {
+      graph.links[i].source = graph.nodes.indexOf(graph.links[i].source);
+      graph.links[i].target = graph.nodes.indexOf(graph.links[i].target);
+    });
+
+    graph.nodes.forEach(function (d, i) {
+      graph.nodes[i] = { "name": d };
+    });
+    
+    sankey(graph);
+          
+    // d3.json("//cdn.rawgit.com/q-m/d3.chart.sankey/master/example/data/product.json")
+    //     .then(function(data: DAG) {
+    //       console.log(data);
+    //   // @ts-ignore
+    //   sankey(data);
+    link = link
+        .data(graph.links)
+        .enter().append("path")
+        .attr("d", d3sankey.sankeyLinkHorizontal())
+        .attr("stroke-width", function (d: any) { return Math.max(1, d.width); });
+
+    link.append("title")
+        .text(function (d: any) { return d.source.name + " → " + d.target.name + "\n" + format(d.value); });
+
+    node = node
+        .data(graph.nodes)
+        .enter().append("g")
+
+    node.append("rect")
+        .attr("x", function (d: any) { return d.x0; })
+        .attr("y", function (d: any) { return d.y0; })
+        .attr("height", function (d: any) { return d.y1 - d.y0; })
+        .attr("width", function (d: any) { return d.x1 - d.x0; })
+        .attr("fill", function (d: any) { return color(d.name.replace(/ .*/, "")); })
+        .attr("stroke", "#000");
+
+    node.append("text")
+        .attr("x", function (d: any) { return d.x0 - 6; })
+        .attr("y", function (d: any) { return (d.y1 + d.y0) / 2; })
+        .attr("dy", "0.35em")
+        .attr("text-anchor", "end")
+        .text(function (d: any) { return d.name + " [" + format(d.value) + ']'; })
+        .filter(function (d: any) { return d.x0 < width / 2; })
+        .attr("x", function (d: any) { return d.x1 + 6; })
+        .attr("text-anchor", "start");
+
+    node.append("title")
+        .text(function (d: any) { return d.name + "\n" + format(d.value); });
+  }
 }
